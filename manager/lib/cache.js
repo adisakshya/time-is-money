658161166e40fd1c646aacb691608d85bf701869
Adisakshya Chauhan <45028982+adisakshya@users.noreply.github.com>
2020-03-15 18:30:32 +0530

    added redis cache middleware

---
+++ manager/lib/cache.js
@@ -0,0 +1,75 @@
+const redis = require("redis");
+const client = redis.createClient({
+    'host': 'redis',
+    'port': 6379
+});
+
+/**
+ * SET key-value in cache
+ * @param {*} key 
+ * @param {*} value 
+ */
+const set = async(key, value) => {
+    return new Promise((resolve, reject) => {
+        client.set(key, value, (err) => {
+            if(err) {
+                reject(err);     
+            } else {
+                resolve(key);
+            }
+        });
+    });
+};
+
+/**
+ * GET key-value from cache
+ * @param {*} key 
+ */
+const get = async(key) => {
+    return new Promise((resolve, reject) => {
+        client.get(key, (err, value)=> {
+            if(err) {
+                reject(err);
+            } else {
+                resolve(value);
+            }
+        });
+    });
+};
+
+/**
+ * DELETE key-value from cache
+ * @param {*} key 
+ */
+const deleteKey = async(key) => {
+    return new Promise((resolve, reject) => {   
+        client.del(key, cb = (err, success) => {
+            if(err) {
+                reject(err);
+            } else {
+                resolve(success);
+            }
+        });
+    });
+};
+
+/**
+ * DELETE all key-value from cache
+ * @param {*} key 
+ */
+const deleteAll = async(key) => {
+    return new Promise((resolve, reject) => {   
+        client.flushall((err, success) => {
+            if(err) {
+                reject(err);
+            } else {
+                resolve(success);
+            }
+        });
+    });
+};
+
+exports.set = set;
+exports.get = get;
+exports.deleteKey = deleteKey;
+exports.deleteAll = deleteAll;
