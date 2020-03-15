5b8b14de3b4463a92927036959f5a71f38799e10
Adisakshya Chauhan <45028982+adisakshya@users.noreply.github.com>
2020-03-15 12:37:58 +0530

    updated model definations for tasks

--- manager/model/tasks/main.js
+++ manager/model/tasks/main.js
@@ -3,10 +3,21 @@
  */
 
 /**
+ * Get connection pool
+ */
+const pool = require('../../rdbms/pool');
+
+/**
  * SELECT all tasks
  */
 const getAllTasks = async () => {
-  
+    pool.query('SELECT * FROM managerdb.tasks', (err, results) => {
+        if (err) {
+          return err;
+        } else {
+          return results;
+        }
+    });
 };
 
 /**
@@ -14,28 +25,52 @@ const getAllTasks = async () => {
  * @param {String} id 
  */
 const getTaskByID = async (id) => {
-  
+    pool.query('SELECT * FROM managerdb.tasks as TASK where TASK.id = ?', [id], (err, results) => {
+        if (err) {
+          return err;
+        } else {
+          return results;
+        }
+    });
 };
 
 /**
  * SELECT all completed tasks
  */
 const getAllCompletedTasks = async () => {
-
+    pool.query('SELECT * FROM managerdb.tasks as TASK where TASK.isCompleted = ?', [1], (err, results) => {
+        if (err) {
+          return err;
+        } else {
+          return results;
+        }
+    });
 };
 
 /**
  * SELECT all paused tasks
  */
 const getAllPausedTasks = async () => {
-  
+    pool.query('SELECT * FROM managerdb.tasks as TASK where TASK.isPaused = ?', [1], (err, results) => {
+        if (err) {
+          return err;
+        } else {
+          return results;
+        }
+    });
 };
 
 /**
  * SELECT all terminated tasks
  */
 const getAllTerminatedTasks = async () => {
-  
+    pool.query('SELECT * FROM managerdb.tasks as TASK where TASK.isTerminated = ?', [1], (err, results) => {
+        if (err) {
+          return err;
+        } else {
+          return results;
+        }
+    });
 };
 
 /**
@@ -51,14 +86,68 @@ const insertNewTask = async (id) => {
  * @param {String} id 
  */
 const deleteTaskByID = async (id) => {
-    
+    pool.getConnection(function(err, connection) {
+        connection.beginTransaction(function(err) {
+            if (err) {                  // Transaction Error
+                connection.rollback(function() {
+                    connection.release();
+                });
+            } else {
+                connection.query('DELETE FROM managerdb.tasks where id = ?', [id], function(err, results) {
+                    if (err) {          // Query Error
+                        connection.rollback(function() {
+                            connection.release();
+                        });
+                    } else {
+                        connection.commit(function(err) {
+                            if (err) {
+                                connection.rollback(function() {
+                                    connection.release();
+                                });
+                            } else {
+                                // Success
+                                connection.release();
+                            }
+                        });
+                    }
+                });
+            }    
+        });
+    });
 };
 
 /**
  * DELETE all tasks
  */
 const deleteAllTasks = async () => {
-  
+    pool.getConnection(function(err, connection) {
+        connection.beginTransaction(function(err) {
+            if (err) {                  // Transaction Error
+                connection.rollback(function() {
+                    connection.release();
+                });
+            } else {
+                connection.query('DELETE FROM managerdb.tasks', function(err, results) {
+                    if (err) {          // Query Error
+                        connection.rollback(function() {
+                            connection.release();
+                        });
+                    } else {
+                        connection.commit(function(err) {
+                            if (err) {
+                                connection.rollback(function() {
+                                    connection.release();
+                                });
+                            } else {
+                                // Success
+                                connection.release();
+                            }
+                        });
+                    }
+                });
+            }    
+        });
+    });
 };
 
 /**
@@ -66,7 +155,34 @@ const deleteAllTasks = async () => {
  * @param {String} id 
  */
 const completeTask = async (id) => {
-  
+    pool.getConnection(function(err, connection) {
+        connection.beginTransaction(function(err) {
+            if (err) {                  // Transaction Error
+                connection.rollback(function() {
+                    connection.release();
+                });
+            } else {
+                connection.query('UPDATE managerdb.tasks as TASK SET TASK.isCompleted = ? WHERE TASK.id = ?;', [1, id], function(err, results) {
+                    if (err) {          // Query Error
+                        connection.rollback(function() {
+                            connection.release();
+                        });
+                    } else {
+                        connection.commit(function(err) {
+                            if (err) {
+                                connection.rollback(function() {
+                                    connection.release();
+                                });
+                            } else {
+                                // Success
+                                connection.release();
+                            }
+                        });
+                    }
+                });
+            }    
+        });
+    });
 };
 
 /**
@@ -74,7 +190,34 @@ const completeTask = async (id) => {
  * @param {String} id 
  */
 const pauseTask = async (id) => {
-  
+    pool.getConnection(function(err, connection) {
+        connection.beginTransaction(function(err) {
+            if (err) {                  // Transaction Error
+                connection.rollback(function() {
+                    connection.release();
+                });
+            } else {
+                connection.query('UPDATE managerdb.tasks as TASK SET TASK.isPaused = ? WHERE TASK.id = ?;', [1, id], function(err, results) {
+                    if (err) {          // Query Error
+                        connection.rollback(function() {
+                            connection.release();
+                        });
+                    } else {
+                        connection.commit(function(err) {
+                            if (err) {
+                                connection.rollback(function() {
+                                    connection.release();
+                                });
+                            } else {
+                                // Success
+                                connection.release();
+                            }
+                        });
+                    }
+                });
+            }    
+        });
+    });
 };
 
 /**
@@ -82,7 +225,34 @@ const pauseTask = async (id) => {
  * @param {String} id 
  */
 const terminateTask = async (id) => {
-  
+    pool.getConnection(function(err, connection) {
+        connection.beginTransaction(function(err) {
+            if (err) {                  // Transaction Error
+                connection.rollback(function() {
+                    connection.release();
+                });
+            } else {
+                connection.query('UPDATE managerdb.tasks as TASK SET TASK.isTerminated = ? WHERE TASK.id = ?;', [1, id], function(err, results) {
+                    if (err) {          // Query Error
+                        connection.rollback(function() {
+                            connection.release();
+                        });
+                    } else {
+                        connection.commit(function(err) {
+                            if (err) {
+                                connection.rollback(function() {
+                                    connection.release();
+                                });
+                            } else {
+                                // Success
+                                connection.release();
+                            }
+                        });
+                    }
+                });
+            }    
+        });
+    });
 };
 
 /**
@@ -90,7 +260,13 @@ const terminateTask = async (id) => {
  * @param {String} id 
  */
 const getTotalRows = async (id) => {
-  
+    pool.query('SELECT totalRows FROM managerdb.tasks as TASK where TASK.id = ?', [id], (err, results) => {
+        if (err) {
+          return err;
+        } else {
+          return results;
+        }
+    });
 };
 
 /**
@@ -98,7 +274,13 @@ const getTotalRows = async (id) => {
  * @param {String} id 
  */
 const getProcessedRows = async (id) => {
-  
+    pool.query('SELECT rowsProcessed FROM managerdb.tasks as TASK where TASK.id = ?', [id], (err, results) => {
+        if (err) {
+          return err;
+        } else {
+          return results;
+        }
+    });
 };
 
 
