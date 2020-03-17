9fb5ef1c7d4f0c7e249b0c93a545c4d52c85f955
Adisakshya Chauhan <45028982+adisakshya@users.noreply.github.com>
2020-03-17 22:24:06 +0530

    added comment description

--- manager/lib/eventEmitter.js
+++ manager/lib/eventEmitter.js
@@ -1,4 +1,16 @@
+/**
+ * Require event module
+ */
 var events = require('events');
+
+/**
+ * Create new event emitter
+ */
 var eventEmitter = new events.EventEmitter();
 
+/**
+ * Export event emitter
+ * to be used for performing
+ * pause/resume/terminate operations
+ */
 module.exports = eventEmitter;
