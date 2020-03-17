22009c63c405953749ca1c6f7258cbce31068b4d
Adisakshya Chauhan <45028982+adisakshya@users.noreply.github.com>
2020-03-17 22:33:08 +0530

    added comment description

--- manager/lib/csvParser.js
+++ manager/lib/csvParser.js
@@ -6,7 +6,7 @@
  */
 
 /**
- * File system and Path utility
+ * Require file system and path utility
  */
 const fs = require('fs');
 const path = require('path');
@@ -25,10 +25,16 @@ const saveToMySQL = require('./saveToMySQL').saveToMySQL;
  * Parse Test CSV
  */
 const parser = async (filename, taskID) => {
+
+    /**
+     * Mark initialization of the task
+     */
     console.log('[TASK] Started ->', taskID);
     
     /**
      * Data Array to store CSV information
+     * This will be used as parameter to perform
+     * insert operation on database
      */
     let dataArray = Array();
 
@@ -55,21 +61,50 @@ const parser = async (filename, taskID) => {
          */
         .on('data', (row) => {
             // Extract row fields from dataArray
+            // 'row' is an object and must be converted into an array
+            // so as to be used in insert operation on database
+
+            // Define empty arrau
             let arr = Array();
+
+            // Set rowID for this row,
+            // this will be the unique id for the row among all rows,
+            // for the same task
             let rowID = dataArray.length + 1;
+
+            // Push taskID and rowID in the array
             arr.push(taskID);
             arr.push(rowID);
+
+            // Convert 'row' object into array object
             let objArr = Array(row);
+
+            // Extract field values using map function
+            // and concatenate with array containing taskID and rowID
+            // Ex.
+            //      arr = [ taskID, rowID ]
+            //
+            //      row = {
+            //          'field1': 'test',
+            //          'field2': 'test',
+            //          'field3': 'test',
+            //          ... so on
+            //      }
+            //      
+            //      fields = [ taskID, rowID, field1, field2, field3, ...so on]
+            //
+            //This fields array can directly be used in the insert operation
             const fields = arr.concat(objArr.map(x => Object.values(x))[0]);
 
+            // Push field array in the dataArray
             dataArray.push(fields);            
         })
         /**
          * On completion of parsing
-         * log number of rows parsed
          */
         .on('end', async (rowCount) => {
-            // console.log(`Parsed ${rowCount} rows`);
+            // Pass dataArray and cooresponding taskID
+            // to be inserted in the database
             saveToMySQL(dataArray, taskID);
         });
 };
